40c9b614a584a699bdab1e257f7145ee07628eb9
SheriffMudasir <sheriffmudasir2021@gmail.com>
2025-11-19 10:26:28 +0100

    Squashed 'intentlink-backend/' content from commit 2e39e9c
    git-subtree-dir: intentlink-backend
    git-subtree-split: 2e39e9cdeb220beebeaac8c6f76eed1611bf3083

---
+++ intentlink_project/urls.py
@@ -0,0 +1,9 @@
+# intentlink_project/urls.py
+from django.contrib import admin
+from django.urls import path
+from .api import api
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', api.urls), 
+]
